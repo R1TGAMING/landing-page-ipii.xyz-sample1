77d55fc83b1f7c037cc7fa40b4a285a231833d66
R1TGAMING <rafisofyangaming1234@gmail.com>
2024-11-25 09:38:53 +0700

    fix bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,16 @@ const Navbar = (props) => {
   const [Navbar, SetNavBar] = useState(false);
 
   return (
-    <div className="navbar bg-slate-100 items-center  w-full shadow-lg shadow-gray-200">
+    <div className="navbar bg-slate-100 items-center static w-full shadow-lg shadow-gray-200">
       <div className="navbar-box flex flex-wrap p-5">
         <div className="logo flex items-center">
           <img src={Logo} className="w-10 h-10 mr-2 opacity-65" />
-          <h1 className="font-bold text-3xl mr-6 text-gray-500 hover:text-gray-600">Ipii.xyz</h1>
+          <h1 className="font-bold text-3xl mr-6 text-gray-500 hover:text-gray-600 drop-shadow-md shadow-gray-600 items-center">ipii.xyz</h1>
         </div>
         {/* Menu for pc */}
-        <ul className="hidden md:flex flex-wrap items-center  text-gray-400">
+        <ul className="hidden md:flex flex-wrap items-center text-gray-400">
           <li>
-            <a href="#" className="mr-6 hover:text-gray-500">
+            <a href="#beranda" className="mr-6 hover:text-gray-500">
               Beranda
             </a>
           </li>
@@ -32,15 +32,9 @@ const Navbar = (props) => {
 
         {/* Menu for mobile */}
         {/* Button */}
-        <div className="md:hidden items-center ml-auto ">
-          <button id="menu-toggle" class="text-gray-800 focus:outline-none" onClick={() => SetNavBar(!Navbar)}>
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+        <div className="md:hidden items-center ml-auto">
+          <button id="menu-toggle" class="text-gray-800 focus:outline-none " onClick={() => SetNavBar(!Navbar)}>
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
           </button>
@@ -49,14 +43,14 @@ const Navbar = (props) => {
       {/* Menu */}
       {Navbar && (
         <div className="md:hidden">
-          <div className="px-5 pt-2 pb-3 space-y-1 text-gray-400 text-bold">
-            <a href="#" className="block  hover:text-gray-500">
+          <div className="px-5 pt-2 pb-3 space-y-1 text-gray-400 text-bold ">
+            <a href="#beranda" className="block  hover:text-gray-500 animate-fade-down animate-delay-100">
               Beranda
             </a>
-            <a href="#" className="block  hover:text-gray-500">
+            <a href="#" className="block  hover:text-gray-500 animate-fade-down animate-delay-150">
               Harga
             </a>
-            <a href="#" className="block  hover:text-gray-500">
+            <a href="#" className="block  hover:text-gray-500 animate-fade-down animate-delay-200">
               Contact
             </a>
           </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,17 +3,17 @@ import Welcome from "../../public/welcome.png";
 
 function HomePage() {
   return (
-    <div className="container mx-auto h-sceeen mt-28">
-      <div className="flex flex-row items-center justify-center content-center">
-        <div className="items-center mr-64 leading-tight ">
+    <div className=" p-3  container mx-auto h-sceeen mt-16 animate-fade animate-delay-350" id="beranda">
+      <div className="md:flex justify-center flex-row items-center content-center">
+        <div className="items-center md:mr-64 mr-5 leading-tight ">
           <h1 className="text-gray-500 font-bold text-7xl drop-shadow-md shadow-gray-600">Welcome</h1>
           <h1 className="text-gray-400 font-bold ml-2 text-3xl drop-shadow-md shadow-gray-600">To ipii.xyz</h1>
           <p className="text-gray-400 ml-3 drop-shadow-md shadow-gray-600">
-            Jasa Layanan Pembuatan Website & Bot Whatsapp, Discord <br></br>Terpercaya 100%
+            Jasa Layanan Pembuatan Website & Bot Whatsapp, Discord. <br></br>Terpercaya 100%
           </p>
         </div>
-        <div>
-          <img src={Welcome} className="h-64 drop-shadow-md shadow-gray-500" />
+        <div className="flex justify-center mt-10">
+          <img src={Welcome} className="sm:mr-20 h-72 drop-shadow-md shadow-gray-500 object-center " />
         </div>
       </div>
     </div>
